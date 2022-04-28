eae88b891245ab2191cf624e25e556845ec04e5d
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-04-28 15:42:45 +0200

    Serialization

--- Django_Rest/backend/api/views.py
+++ Django_Rest/backend/api/views.py
@@ -1,14 +1,18 @@
 import json
 
 from django.forms import model_to_dict
-from django.http import JsonResponse
 
 from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.serializers import ProductSerializer
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data)
-    return JsonResponse(data)
+    if instance:
+        data = ProductSerializer(instance).data
+    return Response(data)
